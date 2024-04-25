ef512c47c7163d1a1f7935774c36813fd188e5ef
Amina Kasa <amina.kasa@gefasoft.com>
2024-04-25 14:43:50 +0200

    Dataset Info - Bilder ausgeben lassen

--- CustomDatasetInfo.py
+++ CustomDatasetInfo.py
@@ -4,6 +4,7 @@ from torchvision.transforms import Compose, ToTensor
 import DefineDataset
 import config
 from torch.utils.data import random_split
+import matplotlib.pyplot as plt
 
 # 1. Datensatz und DataLoader erstellen
 transform = Compose([ToTensor()])  
@@ -76,3 +77,31 @@ masks_tensors_for_image_0 = masks_tensors[index]
 print("Form der Maskentensoren für das Bild mit Index 0:")
 for i, mask_tensor in enumerate(masks_tensors_for_image_0):
     print(f"Maske {i+1}: {mask_tensor.shape}")
+
+print("____________________________________________________________________________________________________________________")
+
+# Funktion zur Visualisierung von Bild und Masken
+def show_image_and_masks(image, masks):
+    plt.figure(figsize=(15, 5))
+    
+    # Bild anzeigen
+    plt.subplot(1, 7, 1)
+    plt.imshow(image.permute(1, 2, 0))  # Permutiere die Dimensionen für die Anzeige
+    plt.title('Bild')
+    plt.axis('off')
+    
+    # Masken anzeigen
+    for i, mask in enumerate(masks):
+        plt.subplot(1, 7, i+2)
+        plt.imshow(mask, cmap='gray')
+        plt.title(f'Maske {i+1}')
+        plt.axis('off')
+    
+    plt.show()
+
+# Lade ein Beispiel aus dem DataLoader
+for data in train_loader:
+    image = data['image'][0]  # Erstes Bild im Batch
+    masks = data['masks'][0]  # Masken zum ersten Bild
+    show_image_and_masks(image, masks)
+    break  # Nur das erste Beispiel anzeigen
