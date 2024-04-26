2a65c64fd9a47bc01046627393ea72bcdb923e41
Amina Kasa <amina.kasa@gefasoft.com>
2024-04-26 13:11:05 +0200

    get_dataloaders() methode

--- DefineDataset.py
+++ DefineDataset.py
@@ -7,51 +7,50 @@ import config
 from torchvision import transforms
 from typing import Tuple, Dict, List
 import matplotlib.pyplot as plt
+from torchvision.transforms import v2
+from torch.utils.data import random_split
+
 
 class CustomDataset(Dataset):
-    def __init__(self, image_folder, mask_folder, transform):
+    def __init__(self, image_folder, mask_folder, transform=None):
         self.image_folder = image_folder
         self.mask_folder = mask_folder
         self.transform = transform
         
         # Liste der Dateinamen in den Ordnern und sortieren nach Bildnummer
-        self.image_files = sorted(os.listdir(image_folder), key=self.extract_image_number)
-        self.mask_files = sorted(os.listdir(mask_folder), key=self.extract_image_number)
+        self.image_files = sorted(os.listdir(image_folder), key=lambda x: int(''.join(filter(str.isdigit, x))))
+        self.mask_files = sorted(os.listdir(mask_folder), key=lambda x: int(''.join(filter(str.isdigit, x))))
         
         # Überprüfen, ob die Anzahl der Dateien übereinstimmt
         # if len(self.image_files) != len(self.mask_files) / 6:
         #     raise ValueError("Die Anzahl der Bilder und Masken stimmt nicht überein.")
-        
-    def extract_image_number(self, file_name):
-         # Extrahiere die numerische Bildnummer aus dem Dateinamen
-         return int(''.join(filter(str.isdigit, file_name)))
-        
+    
     def __len__(self)-> int:
         return len(self.image_files)
     
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
         # Laden des Bildes
         img_name = os.path.join(self.image_folder, self.image_files[index])
-        image = Image.open(img_name)
+        image = Image.open(img_name).convert('RGB')  # Konvertieren zu RGB
         
         # Laden der Masken für dieses Bild
         mask_files_for_image = self.mask_files[index * 6 : (index + 1) * 6]
-        masks = [Image.open(os.path.join(self.mask_folder, mask_file)) for mask_file in mask_files_for_image]
+        masks = [Image.open(os.path.join(self.mask_folder, mask_file)).convert('L') for mask_file in mask_files_for_image]
         
         # Optional: Transformationen anwenden
         if self.transform:
             image = self.transform(image)
             masks = [self.transform(mask) for mask in masks]
             
-        # Konvertieren in Tensor und Rückgabe
-        image_tensor = torch.tensor(np.array(image))
-        masks_tensor = torch.stack([torch.tensor(np.array(mask)) for mask in masks], dim=0)
-        
-        # Anpassung der Dimension der Maskentensoren
-        masks_tensor = masks_tensor.view(-1, masks_tensor.shape[-2], masks_tensor.shape[-1])
+       
+        # Stapeln der Masken und Anpassen der Dimension
+        masks_tensor = torch.stack(masks, dim=0)  # Erzeugt einen Tensor der Form [6, 1, 60, 60]
+
+        # Entfernen der überflüssigen Dimension (die '1' bei der Kanaldimension)
+        masks_tensor = masks_tensor.squeeze(1)  # Ändert die Form zu [6, 60, 60]
 
         return {
-            'image': image_tensor,
+            'image': image,
             'masks': masks_tensor,
         }
 
@@ -79,4 +78,35 @@ def extract_all_tensors(dataset):
         masks_tensors.append(masks_tensor)
 
     return image_tensors, masks_tensors
+
+def get_dataloaders():
+    transform_v2 = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True)])
+
+    train_dataset = CustomDataset(config.IMAGE_DATASET_PATH, config.MASK_DATASET_PATH, transform=transform_v2)
+
+    # Prozentsatz für das Trainingsset ( 80%)
+    train_ratio = 0.8
+
+    # Anzahl der Bilder für das Trainings- und Validierungsset
+    train_size = int(train_ratio * len(train_dataset))
+    val_size = len(train_dataset) - train_size
+
+    # Teile den Datensatz in Trainings- und Validierungsset auf
+    train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
+
+    # DataLoader für Trainings- und Validierungsset
+    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
+
+    #Creating Dataloaders:
+    dataloaders = {
+        'train': train_loader,
+        'val': val_loader
+    }
+
+    return dataloaders
+
+
 
