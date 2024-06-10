b9ce4a4950e429d2f36f8bd17d4fc6b4d0c96dbb
Amina Kasa <amina.kasa@gefasoft.com>
2024-06-10 17:54:29 +0200

    Anpassung an Dataset

--- Run.py
+++ Run.py
@@ -1,30 +1,183 @@
+#%%
+#Import Libraries
+import torch
+import torch.nn as nn
+from torch.nn.functional import relu
+from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler
+import torch.nn.functional as F
 from torchvision import transforms
+from torchvision.utils import save_image
+import numpy as np
+from functools import reduce
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import random
+import itertools
+import time
+import copy
+from PIL import Image
+from tifffile import imwrite
 import DefineDataset
 import config
 import UNetModel
-import torch.nn as nn
 from torch.utils.data import random_split
+from torchvision.transforms import v2
+import GenerateData
+
+# %%
+# DiceLoss Method
+def dice_loss(pred, target, smooth=1.):
+    pred = pred.contiguous()
+    target = target.contiguous()
+
+    intersection = (pred * target).sum(dim=2).sum(dim=2)
+
+    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
+
+    return loss.mean()
+
+#%%
+#Calculate Loss Method
+def calc_loss(pred, target, metrics, bce_weight=0.5):
+    bce = F.binary_cross_entropy_with_logits(pred, target)
+
+    pred = F.sigmoid(pred)
+    dice = dice_loss(pred, target)
+
+    loss = bce * bce_weight + dice * (1 - bce_weight)
+
+    metrics['bce'] += bce.data.cpu().numpy() * target.size(0)
+    metrics['dice'] += dice.data.cpu().numpy() * target.size(0)
+    metrics['loss'] += loss.data.cpu().numpy() * target.size(0)
+
+    return loss
+
+#%% 
+#Print Method
+def print_metrics(metrics, epoch_samples, phase):
+    outputs = []
+    for k in metrics.keys():
+        outputs.append("{}: {:4f}".format(k, metrics[k] / epoch_samples))
+
+    print("{}: {}".format(phase, ", ".join(outputs)))
+
+#%%
+#Train Method
+def train_model(model, optimizer, scheduler, num_epochs):
+    dataloaders = DefineDataset.get_dataloaders()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = 1e10
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        since = time.time()
 
 
-# 1. Datensatz und DataLoader erstellen
-simple_transform = transforms.Compose([ 
-    transforms.Resize((64, 64)),
-    transforms.ToTensor(),
-])
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                for param_group in optimizer.param_groups:
+                    print("LR", param_group['lr'])
 
-train_dataset = DefineDataset.CustomDataset(image_folder=config.IMAGE_DATASET_PATH,
-                        mask_folder=config.MASK_DATASET_PATH,
-                        transform=simple_transform)
+                model.train()
+            else:
+                model.eval()
 
-# Anzahl der Bilder für das Trainings- und Validierungsset
-train_size = int(config.TEST_SPLIT * len(train_dataset))
-val_size = len(train_dataset) - train_size
+            metrics = defaultdict(float)
+            epoch_samples = 0
 
-# Teile den Datensatz in Trainings- und Validierungsset auf
-train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
+            for inputs, labels in dataloaders[phase]:
 
-# DataLoader für Trainings- und Validierungsset
-train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=0)
-val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False, num_workers=0)
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    loss = calc_loss(outputs, labels, metrics)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                epoch_samples += inputs.size(0)
+
+            print_metrics(metrics, epoch_samples, phase)
+            epoch_loss = metrics['loss'] / epoch_samples
+
+            if phase == 'val' and epoch_loss < best_loss:
+                print("saving best model")
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        time_elapsed = time.time() - since
+        print('{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+    print('Best val loss: {:4f}'.format(best_loss))
+
+    model.load_state_dict(best_model_wts)
+    return model
+
+#%%
+#Run Method
+def run(UNet):
+    num_class = 6
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    model = UNet(num_class).to(device)
+
+#Optimizer
+    optimizer_ft = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
+
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=30, gamma=0.1)
+
+    model = train_model(model, optimizer_ft, exp_lr_scheduler, num_epochs=75)
+
+    model.eval()
+
+    # trans = transforms.Compose([
+    #     transforms.ToTensor(),
+    #     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    # ])
+
+    trans = v2.Compose([
+        v2.ToImage(), 
+        v2.ToDtype(torch.float32, scale=True),
+        v2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    
+
+    # # Create another simulation dataset for test
+    test_dataset = DefineDataset.CustomDataset(config.IMAGE_DATASET_PATH, config.MASK_DATASET_PATH, transform=trans,patch_size=60)
+    test_loader = DefineDataset.DataLoader(test_dataset, batch_size=3, shuffle=False, num_workers=0)
+
+    inputs, labels = next(iter(test_loader))
+    inputs = inputs.to(device)
+    labels = labels.to(device)
+
+    pred = model(inputs)
+    pred = F.sigmoid(pred)
+    pred = pred.data.cpu().numpy()
+    print(pred.shape)
+
+    # Change channel-order and make 3 channels for matplot
+    input_images_rgb = [GenerateData.reverse_transform(x) for x in inputs.cpu()]
+    
+    # Map each channel (i.e. class) to each color
+    target_masks_rgb = [GenerateData.masks_to_colorimg(x) for x in labels.cpu().numpy()]
+    
+    pred_rgb = [GenerateData.masks_to_colorimg(x) for x in pred]
+    
+
+    GenerateData.plot_side_by_side([input_images_rgb, target_masks_rgb, pred_rgb])
+
+#%%
+#Run Model
+run(UNetModel.UNet)
